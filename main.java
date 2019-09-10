4bc00a115320e190d3fa6b9ceff11b2f3cddafa2
GabrielDeCastro <30298508+GabrielDCastro@users.noreply.github.com>
2019-09-10 20:58:57 -0300

    Update Class Luta

--- main.java
+++ main.java
@@ -3,7 +3,7 @@ public class javaapplication1{
     public static void main(String[] args){
         Lutador l[] = new Lutador[6];
         l[0] = new Lutador("Gabriel", "Brasil", 20, 10, 2, 1.70f, 60f);
-        l[1] = new Lutador("Marcelo", "EUA", 21, 14, 3, 1.80f, 80f);
+        l[1] = new Lutador("Marcelo", "EUA", 21, 14, 3, 1.80f, 70f);
         
         for (int i=0 ; i<=1; i++){
           l[i].status();
@@ -12,7 +12,10 @@ public class javaapplication1{
         l[1].apresentar();
         l[1].ganharluta();
         
-        for (int i=0 ; i<=1; i++){
+        Luta UFC1 = new Luta();
+        UFC1.marcarLuta(l[0], l[1]);
+        UFC1.lutar();
+          for (int i=0 ; i<=1; i++){
           l[i].apresentar();
         }
     }
